6dcf4e5164aea31ad6c436dd95bd39819ff56058
Rafsan Hasan Pronoy <rafsanhasanpronoy00@gmail.com>
2026-09-20 13:00:20 +0600

    Add centralized API exception handler

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -108,6 +108,7 @@ MEDIA_ROOT = BASE_DIR / "media"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
+    "EXCEPTION_HANDLER": "store.exceptions.custom_exception_handler",
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
